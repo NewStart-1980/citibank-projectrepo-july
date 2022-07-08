87ce89d007a9436b0ff92beb2661165816ea3aab
Newstart-1980 <dab.niharika@gmail.com>
2022-07-08 18:39:33 -0500

    feature302 changes in app.java by dev2

--- app.java
+++ app.java
@@ -22,4 +22,13 @@ class Main {
       e.getStackTrace();
     }
   }
+  #feature302 changes bt dev2"
+  public static void sampleMethod(int a, int b) throws ArithmeticException{
+        System.out.println("Hello, this is sample method");
+        int c = a/b; //  int c = 4/2 which gets calculated as 2 
+        System.out.println("c:"+c); // c: 2
+   }
+  
+  
+  
 }
